2bac3e004e221bced8a2b233db26d0cc54af9e17
Naveen6612 <kumarnaveen.nk2004@gmail.com>
2026-04-04 07:30:59 +0530

    fixed final pages + fixes

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,20 +1,19 @@
 "use client";
-import ChartsSection from "@/components/ChartsSection";
 
+import ChartsSection from "@/components/ChartsSection";
 import SummaryCards from "@/components/SummaryCard";
 import Topbar from "@/components/Topbar";
 import TransactionsSection from "@/components/TransactionSection";
 
 export default function Home() {
   return (
-    <div className="flex h-screen overflow-hidden bg-gray-50 min-h-screen">
-      <div className="flex-1 p-6 overflow-y-auto">
+    <div className="flex flex-col min-h-screen bg-gray-50">
+      <div className="flex-1 p-6 space-y-6">
         <Topbar />
 
         <SummaryCards />
         <ChartsSection />
         <TransactionsSection />
-        {/* Next sections will go here */}
       </div>
     </div>
   );
--- apps/web/src/components/ChartsSection.tsx
+++ apps/web/src/components/ChartsSection.tsx
@@ -38,7 +38,7 @@ export default function ChartsSection() {
 
   if (!mounted) return null;
   return (
-    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 sm:gap-6 mb-6 h-[300px] w-full">
+    <div className="grid grid-cols-1 lg:grid-cols-3 gap-4 sm:gap-6 mb-6 w-full">
       {/*  Line Chart */}
       <div className="lg:col-span-2 bg-white border border-gray-200 rounded-xl p-4 sm:p-5">
         <h3 className="text-sm font-medium text-gray-700 mb-3 sm:mb-4">
@@ -63,7 +63,6 @@ export default function ChartsSection() {
           </ResponsiveContainer>
         </div>
       </div>
-
       {/*  Pie Chart */}
       <div className="bg-white border border-gray-200 rounded-xl p-4 sm:p-5 flex flex-col">
         <h3 className="text-sm font-medium text-gray-700 mb-3 sm:mb-4">
